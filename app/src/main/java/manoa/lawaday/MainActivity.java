7f10bec657ced0618190719589780688a60ed611
sunquan8094 <sunquan8094@w-105computer24.iolani.iolani.org>
2015-02-13 23:10:02 -1000

    Revert c9c1d83..db35b98
    This rolls back to commit c9c1d83a734ae361f7bf6503d86ca1097d4538f3.

--- app/src/main/java/manoa/lawaday/MainActivity.java
+++ app/src/main/java/manoa/lawaday/MainActivity.java
@@ -1,6 +1,7 @@
 package manoa.lawaday;
 
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,14 +9,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.os.Build;
 
 
-public class MainActivity2 extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
@@ -57,7 +59,7 @@ public class MainActivity2 extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main2, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
     }
