aa0a38385e48df0509d132591b6c40597d565e59
sunquan8094 <sunquan8094@w-105computer24.iolani.iolani.org>
2015-02-14 00:54:26 -1000

    Finally getting somewhere...

--- app/src/main/java/manoa/lawaday/MainActivity.java
+++ app/src/main/java/manoa/lawaday/MainActivity.java
@@ -1,30 +1,35 @@
 package manoa.lawaday;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public void accessPage(View view) {
+        if (view.equals(findViewById(R.id.button))) {
+            Intent i = new Intent(this, MainActivity2.class);
+            startActivity(i);
+        }
+        else if (view.equals(findViewById(R.id.button2))) {
+            Intent i = new Intent(this, MainActivity2.class);
+            startActivity(i);
+        }
+        else if (view.equals(findViewById(R.id.button3))) {
+            Intent i = new Intent(this, MainActivity2.class);
+            startActivity(i);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,21 +51,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
     }
 }
